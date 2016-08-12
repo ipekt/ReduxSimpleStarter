a6a71b15c7ec8ec186f553af4a9acdb430c15633
ipek <ipek.turgul@gmail.com>
2016-08-12 16:42:12 +1000

    Build redux book list app

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import config from './webpack.config.dev';
-import open from 'open';
+// import open from 'open';
 
-const port = 3000;
-let targetEntry = 'http://localhost:' + port + '/';
+let targetEntry = 'http://localhost:3000/';
 config.entry.unshift("webpack-dev-server/client?" + targetEntry);
 
-new WebpackDevServer(webpack(config), {hot: true, inline: true, stats: {colors: true}})
-  .listen(port, 'localhost', (err) => {
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(3000, 'localhost', (err) => {
     if (err) {
       console.log(err);
     }
-    console.log('Listening at localhost:' + port);
-    console.log('Opening your system browser...');
-    open(targetEntry);
+    console.log('Listening at localhost:3000');
+    // console.log('Opening your system browser...');
+    // open(targetEntry);
   });
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,27 +4,25 @@ import path from 'path';
 export default {
   debug: true,
   devtool: 'cheap-module-eval-source-map',
-  noInfo: true,
   entry: [
-    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    './src/index.js'
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), exclude: /node_modules/, loaders: ['babel']},
+    loaders: [{
+      test: /\.js$/,
+      include: path.join(__dirname, 'src'),
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel']},
     ]
   },
 };
